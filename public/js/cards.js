3013178315cc32b014d4c5181ec25b2dcbcc0928
Abduvali <abduvalikozimov30012006@gmail.com>
2024-03-17 01:30:47 +0500

    first commit

--- public/js/cards.js
+++ public/js/cards.js
@@ -84,12 +84,11 @@ const filterCards = e => {
   const cards = document.querySelectorAll('.card');
 
   cards.forEach(card => {
-    // card.style.display = 'none';
-    card.classList.add("hidden")
+    card.style.display = 'none';
+    // card.classList.add("hidden")
     if (filterName === 'all' || card.dataset.name === filterName) {
-      // card.style.display = 'block';
-    card.classList.add("block")
-
+      card.style.display = 'block';
+    // card.classList.add("block")
     }
   });
 }
